4cdc8b396919d46895388b79035edbef1e120a8b
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-0>
2025-02-08 04:41:33 +0000

    Reset to 24479d0341ce3fd94ec20fe71d70e6c2fad70c50 and made changes for LLM Round 1

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,1 @@
-from src.utils import url_join\\ndef flatten(arg):\\"""Flattens a nested list."""\\\\\n  if not isinstance(arg, list):\\\\\n    return [arg]\\\\\n  return [x for sub in arg for x in flatten(sub)]
+from src.utils import url_join\ndef flatten(arg):\"\"\"Flattens a nested list.\"\"\"\n  if not isinstance(arg, list):\n    return [arg]\n  return [x for sub in arg for x in flatten(sub)]
