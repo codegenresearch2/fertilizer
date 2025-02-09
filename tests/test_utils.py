e43e0097aa3c48a8ea1163f7b8bb63eea9b26787
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-09 16:13:02 +0000

    Reset to 24479d0341ce3fd94ec20fe71d70e6c2fad70c50 and made changes for LLM Round 1

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,33 +1,24 @@
 from .helpers import SetupTeardown
 
-from src.utils import flatten, url_join
-
-class TestFlatten(SetupTeardown):
-  def test_flattens_list(self):
-    assert flatten([1, [2, 3], 4]) == [1, 2, 3, 4]
-
-  def test_returns_already_flat_list(self):
-    assert flatten([1, 2, 3]) == [1, 2, 3]
-
+from src.utils import url_join
 
 class TestUrlJoin(SetupTeardown):
-  def test_joins_paths(self):
-    assert url_join('/tmp', 'test', 'file') == '/tmp/test/file'
-
-  def test_joins_paths_when_some_have_leading_slash(self):
-    assert url_join('/tmp', '/test', 'file') == '/tmp/test/file'
-
-  def test_joins_paths_with_multiple_slashes(self):
-    assert url_join('/tmp', '/test/', '/file') == '/tmp/test/file'
-
-  def test_joins_paths_with_trailing_slash(self):
-    assert url_join('/tmp/', 'test/', 'file/') == '/tmp/test/file'
-
-  def test_joins_paths_with_empty_strings(self):
-    assert url_join('', 'test', '') == 'test'
+  def test_joins_paths_with_leading_slashes(self):
+    result = url_join('/tmp', '/test', '/file')
+    assert result == '/tmp/test/file'
 
   def test_joins_paths_with_none_values(self):
-    assert url_join(None, 'test', None) == 'test'
+    result = url_join(None, 'test', None)
+    assert result == 'test'
 
-  def test_joins_paths_with_mixed_types(self):
-    assert url_join('/tmp', None, 'test', '', 'file') == '/tmp/test/file'
+  def test_joins_full_uri(self):
+    result = url_join('http://example.com', 'path', 'to', 'resource')
+    assert result == 'http://example.com/path/to/resource'
+
+  def test_strips_bare_slashes(self):
+    result = url_join('/tmp', '/test/', '/file/')
+    assert result == '/tmp/test/file'
+
+  def test_handles_leading_and_trailing_slashes(self):
+    result = url_join('/tmp/', 'test/', 'file/')
+    assert result == '/tmp/test/file'
