e0c4c7f3aeda1db31bd4fa12b545ba88f0fb743e
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-09 15:47:36 +0000

    Reset to 24479d0341ce3fd94ec20fe71d70e6c2fad70c50 and made changes for LLM Round 1

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,11 +1,11 @@
 from .helpers import SetupTeardown
 
-from src.utils import url_join
+from src.utils import flatten
 
 
 class TestFlatten(SetupTeardown):
   def test_flattens_list(self):
-    assert url_join(1, [2, 3], 4) == [1, 2, 3, 4]
+    assert flatten([1, [2, 3], 4]) == [1, 2, 3, 4]
 
   def test_returns_already_flat_list(self):
-    assert url_join(1, 2, 3) == [1, 2, 3]
+    assert flatten([1, 2, 3]) == [1, 2, 3]
