693ad5cc32cbeae9160ea7b8254a87e9db6671b7
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-09 16:00:27 +0000

    Reset to 24479d0341ce3fd94ec20fe71d70e6c2fad70c50 and made changes for LLM Round 1

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,6 @@
 from .helpers import SetupTeardown
 
-from src.utils import flatten
-
+from src.utils import flatten, url_join
 
 class TestFlatten(SetupTeardown):
   def test_flattens_list(self):
@@ -9,3 +8,26 @@ class TestFlatten(SetupTeardown):
 
   def test_returns_already_flat_list(self):
     assert flatten([1, 2, 3]) == [1, 2, 3]
+
+
+class TestUrlJoin(SetupTeardown):
+  def test_joins_paths(self):
+    assert url_join('/tmp', 'test', 'file') == '/tmp/test/file'
+
+  def test_joins_paths_when_some_have_leading_slash(self):
+    assert url_join('/tmp', '/test', 'file') == '/tmp/test/file'
+
+  def test_joins_paths_with_multiple_slashes(self):
+    assert url_join('/tmp', '/test/', '/file') == '/tmp/test/file'
+
+  def test_joins_paths_with_trailing_slash(self):
+    assert url_join('/tmp/', 'test/', 'file/') == '/tmp/test/file'
+
+  def test_joins_paths_with_empty_strings(self):
+    assert url_join('', 'test', '') == 'test'
+
+  def test_joins_paths_with_none_values(self):
+    assert url_join(None, 'test', None) == 'test'
+
+  def test_joins_paths_with_mixed_types(self):
+    assert url_join('/tmp', None, 'test', '', 'file') == '/tmp/test/file'
