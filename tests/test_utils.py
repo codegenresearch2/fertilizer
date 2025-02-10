fd2f7d942ab8cfbb57de1e1b8726e53f0593fe94
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-10 00:54:32 +0000

    Reset to 24479d0341ce3fd94ec20fe71d70e6c2fad70c50 and made changes for LLM Round 1

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,5 @@
 from .helpers import SetupTeardown
-from src.utils import flatten
+from src.utils import flatten, url_join
 
 class TestFlatten(SetupTeardown):
   def test_flattens_single_level_list(self):
@@ -19,3 +19,36 @@ class TestFlatten(SetupTeardown):
 
   def test_handles_none_input(self):
     assert flatten(None) == [None]
+
+class TestUrlJoin(SetupTeardown):
+  def test_joins_paths(self):
+    path = url_join("/tmp", "test", "file")
+    assert path == "/tmp/test/file"
+
+  def test_joins_paths_with_leading_slash(self):
+    path = url_join("/tmp", "/test", "file")
+    assert path == "/tmp/test/file"
+
+  def test_joins_paths_with_trailing_slash(self):
+    path = url_join("/tmp", "test/", "file")
+    assert path == "/tmp/test/file"
+
+  def test_joins_full_urls(self):
+    path = url_join("http://example.com", "test", "file")
+    assert path == "http://example.com/test/file"
+
+  def test_joins_full_urls_with_trailing_slash(self):
+    path = url_join("http://example.com/", "test", "file")
+    assert path == "http://example.com/test/file"
+
+  def test_joins_full_urls_with_leading_slash(self):
+    path = url_join("http://example.com/", "/test", "file")
+    assert path == "http://example.com/test/file"
+
+  def test_handles_empty_parts(self):
+    path = url_join("http://example.com", "", "test", "")
+    assert path == "http://example.com/test"
+
+  def test_handles_only_slashes(self):
+    path = url_join("http://example.com", "/", "/", "/")
+    assert path == "http://example.com"
