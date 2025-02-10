803e59c7895265a52bf3a2f30185d1c96e80a433
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-10 02:04:04 +0000

    Reset to 24479d0341ce3fd94ec20fe71d70e6c2fad70c50 and made changes for LLM Round 1

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,17 +2,20 @@ from .helpers import SetupTeardown
 from src.utils import flatten
 
 class TestFlatten(SetupTeardown):
-  def test_flattens_nested_list(self):
+  def test_flattens_single_level_list(self):
+    assert flatten([1, [2, 3], 4]) == [1, 2, 3, 4]
+
+  def test_returns_same_list_if_already_flat(self):
+    assert flatten([1, 2, 3]) == [1, 2, 3]
+
+  def test_flattens_deeply_nested_list(self):
     assert flatten([1, [2, [3, 4]], 5]) == [1, 2, 3, 4, 5]
 
-  def test_returns_single_element_list(self):
-    assert flatten(6) == [6]
+  def test_handles_non_list_elements(self):
+    assert flatten([1, [2, "three"], 4]) == [1, 2, "three", 4]
 
   def test_returns_empty_list_for_empty_input(self):
     assert flatten([]) == []
 
   def test_flattens_empty_nested_list(self):
     assert flatten([[], [[]], [1, 2, 3]]) == [1, 2, 3]
-
-  def test_handles_non_list_elements(self):
-    assert flatten([1, [2, "three"], 4]) == [1, 2, "three", 4]
