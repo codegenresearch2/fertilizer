974e772005b0a1a6fe50b0139223d704b5831c82
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-10 00:28:17 +0000

    Reset to 24479d0341ce3fd94ec20fe71d70e6c2fad70c50 and made changes for LLM Round 1

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,18 +2,18 @@ from .helpers import SetupTeardown
 from src.utils import url_join
 
 class TestUrlJoin(SetupTeardown):
-  def test_joins_paths_without_trailing_slash(self):
+  def test_join_urls(self):
     url = url_join("http://example.com", "path", "to", "resource")
     assert url == "http://example.com/path/to/resource"
 
-  def test_joins_paths_with_trailing_slash(self):
+  def test_join_urls_with_trailing_slash(self):
     url = url_join("http://example.com/", "path", "to", "resource")
     assert url == "http://example.com/path/to/resource"
 
-  def test_joins_paths_with_leading_slash(self):
+  def test_join_urls_with_leading_slash(self):
     url = url_join("http://example.com", "/path", "to", "resource")
     assert url == "http://example.com/path/to/resource"
 
-  def test_joins_paths_with_empty_parts(self):
+  def test_join_urls_with_empty_parts(self):
     url = url_join("http://example.com", "", "path", "", "to", "resource")
     assert url == "http://example.com/path/to/resource"
