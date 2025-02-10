46948c626c361a7927ce25cec0b7fab47341e6a5
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-10 22:42:48 +0000

    Reset to 56f82451c924e0e271325c1ac17b98910260a737 and made changes for LLM Round 1

--- src/errors.py
+++ src/errors.py
@@ -1,3 +1,10 @@
+from src.errors import handle_error
+
 class ConfigKeyError(Exception):
     """Exception raised for errors in the configuration key."""
-    pass
+    def __init__(self, key, message="Configuration key error: '{}' not found"):
+        self.key = key
+        self.message = message.format(key)
+        super().__init__(self.message)
+
+# Additional imports can be added here if necessary
