ba097fee1836958296ee6249e846bdfd328b69dd
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-10 01:14:15 +0000

    Reset to 7609b7389508be1c93e0c214d4d3c36e435081d5 and made changes for LLM Round 1

--- tests/test_config.py
+++ tests/test_config.py
@@ -31,3 +31,12 @@ class TestConfig(SetupTeardown):
 
         assert "Key 'red_key' not found in config file." in str(excinfo.value)
         os.remove("/tmp/empty.json")
+
+    def test_returns_default_value_if_present(self):
+        with open("/tmp/empty.json", "w") as f:
+            f.write("{}")
+
+        config = Config().load("/tmp/empty.json")
+
+        assert config.server_port == "9713"
+        os.remove("/tmp/empty.json")
