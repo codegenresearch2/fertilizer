0d9a1fdfe77aec3b903d34550eb994ca756bcf3a
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-10 21:27:29 +0000

    Reset to 7609b7389508be1c93e0c214d4d3c36e435081d5 and made changes for LLM Round 1

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,42 +2,31 @@ import os
 import pytest
 
 from .support import SetupTeardown
-
 from src.config import Config
 from src.errors import ConfigKeyError
 
 
 class TestConfig(SetupTeardown):
-  def test_loads_config(self):
-    config = Config().load("tests/support/settings.json")
+    def test_loads_config(self):
+        config = Config().load("tests/support/settings.json")
 
-    assert config.red_key == "red_key"
-    assert config.ops_key == "ops_key"
+        assert config.red_key == "secret_red"
+        assert config.ops_key == "secret_ops"
 
-  def test_raises_error_on_missing_config_file(self):
-    with pytest.raises(FileNotFoundError) as excinfo:
-      Config().load("tests/support/missing.json")
+    def test_raises_error_on_missing_config_file(self):
+        with pytest.raises(FileNotFoundError) as excinfo:
+            Config().load("tests/support/missing.json")
 
-    assert "tests/support/missing.json does not exist" in str(excinfo.value)
+        assert "tests/support/missing.json does not exist" in str(excinfo.value)
 
-  def test_raises_error_on_missing_key_without_default(self):
-    with open("/tmp/empty.json", "w") as f:
-      f.write("{}")
+    def test_raises_error_on_missing_key(self):
+        with open("/tmp/empty.json", "w") as f:
+            f.write("{}")
 
-    config = Config().load("/tmp/empty.json")
+        config = Config().load("/tmp/empty.json")
 
-    with pytest.raises(ConfigKeyError) as excinfo:
-      config.red_key
+        with pytest.raises(ConfigKeyError) as excinfo:
+            config.red_key
 
-    assert "Key 'red_key' not found in config file." in str(excinfo.value)
-    os.remove("/tmp/empty.json")
-
-  def test_returns_default_value_if_present(self):
-    with open("/tmp/empty.json", "w") as f:
-      f.write("{}")
-
-    config = Config().load("/tmp/empty.json")
-
-    assert config.server_port == "9713"
-
-    os.remove("/tmp/empty.json")
+        assert "Key 'red_key' not found in config file." in str(excinfo.value)
+        os.remove("/tmp/empty.json")
