f9d35ff50d424c084e8019823246f6ac6c31c7d6
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-10 00:41:37 +0000

    Reset to 24479d0341ce3fd94ec20fe71d70e6c2fad70c50 and made changes for LLM Round 1

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,19 +1,21 @@
 from .helpers import SetupTeardown
-from src.utils import url_join
+from src.utils import flatten
 
-class TestUrlJoin(SetupTeardown):
-  def test_join_urls(self):
-    url = url_join("http://example.com", "path", "to", "resource")
-    assert url == "http://example.com/path/to/resource"
+class TestFlatten(SetupTeardown):
+  def test_flattens_single_level_list(self):
+    assert flatten([1, [2, 3], 4]) == [1, 2, 3, 4]
 
-  def test_join_urls_with_trailing_slash(self):
-    url = url_join("http://example.com/", "path", "to", "resource")
-    assert url == "http://example.com/path/to/resource"
+  def test_returns_already_flat_list(self):
+    assert flatten([1, 2, 3]) == [1, 2, 3]
 
-  def test_join_urls_with_leading_slash(self):
-    url = url_join("http://example.com", "/path", "to", "resource")
-    assert url == "http://example.com/path/to/resource"
+  def test_flattens_nested_list(self):
+    assert flatten([1, [2, [3, 4]], 5]) == [1, 2, 3, 4, 5]
 
-  def test_join_urls_with_empty_parts(self):
-    url = url_join("http://example.com", "", "path", "", "to", "resource")
-    assert url == "http://example.com/path/to/resource"
+  def test_handles_empty_list(self):
+    assert flatten([]) == []
+
+  def test_handles_non_list_input(self):
+    assert flatten(1) == [1]
+
+  def test_handles_none_input(self):
+    assert flatten(None) == [None]
