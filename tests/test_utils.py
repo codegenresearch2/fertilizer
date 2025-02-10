28a5b50f7c76a2dd6e30b2ed736f812976c9d09c
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-10 21:44:28 +0000

    Reset to 24479d0341ce3fd94ec20fe71d70e6c2fad70c50 and made changes for LLM Round 1

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,5 @@
 from .helpers import SetupTeardown
-
 from src.utils import url_join, flatten
-
 
 class TestFlatten(SetupTeardown):
   def test_flattens_list(self):
@@ -9,3 +7,22 @@ class TestFlatten(SetupTeardown):
 
   def test_returns_already_flat_list(self):
     assert flatten([1, 2, 3]) == [1, 2, 3]
+
+class TestUrlJoin:
+  def test_joins_paths(self):
+    assert url_join("example.com", "path", "to", "resource") == "example.com/path/to/resource"
+
+  def test_joins_paths_with_leading_slash(self):
+    assert url_join("example.com", "/path", "to", "resource") == "example.com/path/to/resource"
+
+  def test_joins_paths_with_trailing_slash(self):
+    assert url_join("example.com", "path", "to", "/resource") == "example.com/path/to/resource"
+
+  def test_joins_full_uri(self):
+    assert url_join("http://example.com", "path", "to", "resource") == "http://example.com/path/to/resource"
+
+  def test_joins_with_empty_parts(self):
+    assert url_join("example.com", "", "path", "", "resource") == "example.com/path/resource"
+
+  def test_joins_with_multiple_slashes(self):
+    assert url_join("example.com", "path//to", "resource") == "example.com/path/to/resource"
