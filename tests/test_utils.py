e5bff850d69ccf75535e3524e69edb092e5aade0
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-10 02:17:29 +0000

    Reset to 24479d0341ce3fd94ec20fe71d70e6c2fad70c50 and made changes for LLM Round 1

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,21 +1,27 @@
 from .helpers import SetupTeardown
-from src.utils import flatten
+from src.utils import url_join
 
-class TestFlatten(SetupTeardown):
-  def test_flattens_single_level_list(self):
-    assert flatten([1, [2, 3], 4]) == [1, 2, 3, 4]
+class TestUrlJoin(SetupTeardown):
+  def test_joins_simple_urls(self):
+    assert url_join("http://example.com", "page") == "http://example.com/page"
 
-  def test_returns_same_list_if_already_flat(self):
-    assert flatten([1, 2, 3]) == [1, 2, 3]
+  def test_trims_trailing_slashes(self):
+    assert url_join("http://example.com/", "page/") == "http://example.com/page"
 
-  def test_flattens_deeply_nested_list(self):
-    assert flatten([1, [2, [3, 4]], 5]) == [1, 2, 3, 4, 5]
+  def test_handles_multiple_args(self):
+    assert url_join("http://example.com", "page", "subpage") == "http://example.com/page/subpage"
 
-  def test_handles_non_list_elements(self):
-    assert flatten([1, [2, "three"], 4]) == [1, 2, "three", 4]
+  def test_trims_leading_slashes(self):
+    assert url_join("/http://example.com", "page") == "http://example.com/page"
 
-  def test_returns_empty_list_for_empty_input(self):
-    assert flatten([]) == []
+  def test_handles_no_scheme(self):
+    assert url_join("example.com", "page") == "example.com/page"
 
-  def test_flattens_empty_nested_list(self):
-    assert flatten([[], [[]], [1, 2, 3]]) == [1, 2, 3]
+  def test_handles_trailing_slash_on_last_arg(self):
+    assert url_join("http://example.com", "page/") == "http://example.com/page"
+
+  def test_handles_empty_args(self):
+    assert url_join("http://example.com", "", "page") == "http://example.com/page"
+
+  def test_handles_multiple_empty_args(self):
+    assert url_join("http://example.com", "", "", "page") == "http://example.com/page"
