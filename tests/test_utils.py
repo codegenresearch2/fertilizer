f049a4c7b65aad1e057ca29b5028da62887f00e7
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 09:02:53 +0000

    Reset to 24479d0341ce3fd94ec20fe71d70e6c2fad70c50 and made changes for LLM Round 1

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,25 +6,20 @@ from src.utils import url_join
 class TestUrlJoin(SetupTeardown):
   def test_joins_paths_without_none(self):
     path = url_join("http://example.com", "test", "file")
-
     assert path == "http://example.com/test/file"
 
   def test_joins_paths_with_leading_slash(self):
     path = url_join("http://example.com", "/test", "file")
-
     assert path == "http://example.com/test/file"
 
   def test_joins_paths_with_trailing_slash(self):
     path = url_join("http://example.com/", "test", "file")
-
     assert path == "http://example.com/test/file"
 
   def test_joins_paths_with_empty_parts(self):
     path = url_join("http://example.com", "", "file")
-
     assert path == "http://example.com/file"
 
   def test_joins_paths_with_none_parts(self):
     path = url_join("http://example.com", None, "file")
-
     assert path == "http://example.com/file"
