f4307e5c1b9501f304f3299028697cfd7a0de888
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-11 13:23:26 +0000

    Reset to 24479d0341ce3fd94ec20fe71d70e6c2fad70c50 and made changes for LLM Round 1

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,50 +3,16 @@ from .helpers import SetupTeardown
 from src.utils import flatten, url_join
 
 class TestUrlJoin(SetupTeardown):
-  def test_joins_paths_without_leading_or_trailing_slashes(self):
-    path = url_join('api', 'v1', 'users')
-    assert path == 'api/v1/users'
-
-  def test_joins_paths_with_leading_slashes(self):
-    path = url_join('/api', 'v1', 'users')
-    assert path == '/api/v1/users'
-
-  def test_joins_paths_with_trailing_slashes(self):
-    path = url_join('api/', 'v1/', 'users/')
-    assert path == 'api/v1/users/'
-
-  def test_joins_paths_with_mixed_slashes(self):
-    path = url_join('/api', 'v1/', 'users/')
-    assert path == '/api/v1/users/'
-
-  def test_joins_full_uris(self):
-    path = url_join('https://api.example.com', 'v1', 'users')
-    assert path == 'https://api.example.com/v1/users'
-
-  def test_joins_full_uris_with_leading_slashes(self):
-    path = url_join('https://api.example.com/', 'v1', 'users')
-    assert path == 'https://api.example.com/v1/users'
-
-  def test_joins_full_uris_with_trailing_slashes(self):
-    path = url_join('https://api.example.com/', 'v1/', 'users/')
-    assert path == 'https://api.example.com/v1/users/'
-
-  def test_joins_full_uris_with_mixed_slashes(self):
-    path = url_join('https://api.example.com/', 'v1/', 'users')
-    assert path == 'https://api.example.com/v1/users'
-
-  def test_handles_empty_strings(self):
-    path = url_join('', 'v1', 'users')
-    assert path == 'v1/users'
-
-  def test_handles_empty_strings_with_leading_slashes(self):
-    path = url_join('', '/v1', 'users')
-    assert path == '/v1/users'
-
-  def test_handles_empty_strings_with_trailing_slashes(self):
-    path = url_join('', 'v1/', 'users')
-    assert path == 'v1/users'
-
-  def test_handles_empty_strings_with_mixed_slashes(self):
-    path = url_join('', '/v1/', 'users/')
-    assert path == '/v1/users/'
+  def test_joins_paths(self):
+    assert url_join('api', 'v1', 'users') == 'api/v1/users'
+    assert url_join('/api', 'v1', 'users') == '/api/v1/users'
+    assert url_join('api/', 'v1/', 'users/') == 'api/v1/users/'
+    assert url_join('/api', 'v1/', 'users/') == '/api/v1/users/'
+    assert url_join('https://api.example.com', 'v1', 'users') == 'https://api.example.com/v1/users'
+    assert url_join('https://api.example.com/', 'v1', 'users') == 'https://api.example.com/v1/users'
+    assert url_join('https://api.example.com/', 'v1/', 'users/') == 'https://api.example.com/v1/users/'
+    assert url_join('https://api.example.com/', 'v1/', 'users') == 'https://api.example.com/v1/users'
+    assert url_join('', 'v1', 'users') == 'v1/users'
+    assert url_join('', '/v1', 'users') == '/v1/users'
+    assert url_join('', 'v1/', 'users') == 'v1/users'
+    assert url_join('', '/v1/', 'users/') == '/v1/users/'
