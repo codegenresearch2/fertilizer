b05359c02730de41fcfa540d1c29c8c70a53040f
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 15:52:41 +0000

    Reset to 7609b7389508be1c93e0c214d4d3c36e435081d5 and made changes for LLM Round 1

--- main.py
+++ main.py
@@ -13,13 +13,7 @@ def cli_entrypoint(args):
         red_api, ops_api = __verify_api_keys(config)
 
         if args.server:
-            run_webserver(
-                args.input_directory,
-                args.output_directory,
-                red_api,
-                ops_api,
-                port=config.server_port
-            )
+            run_webserver(args.input_directory, args.output_directory, red_api, ops_api, port=config.server_port)
         elif args.input_file:
             print(scan_torrent_file(args.input_file, args.output_directory, red_api, ops_api))
         elif args.input_directory:
