a429baa416578f8f16e51ee8d4ef5b82d9a7dd94
vatsal_venkatkrishna <vatsal_venkatkrishna@gcpl4-eu-0>
2025-02-14 12:01:24 +0000

    Reset to 2413cdd83206cc80bf748df95773de4b2f0407d2 and made changes for LLM Round 1

--- src/trackers.py
+++ src/trackers.py
@@ -1,64 +1,65 @@
 class Tracker:
-  @staticmethod
-  def source_flags_for_search():
-    raise NotImplementedError
+    @staticmethod
+    def source_flags_for_search():
+        raise NotImplementedError
 
-  @staticmethod
-  def source_flags_for_creation():
-    raise NotImplementedError
+    @staticmethod
+    def source_flags_for_creation():
+        raise NotImplementedError
 
-  @staticmethod
-  def announce_url():
-    raise NotImplementedError
+    @staticmethod
+    def announce_url():
+        raise NotImplementedError
 
-  @staticmethod
-  def site_shortname():
-    raise NotImplementedError
+    @staticmethod
+    def site_shortname():
+        raise NotImplementedError
 
-  @staticmethod
-  def reciprocal_tracker():
-    raise NotImplementedError
-
+    @staticmethod
+    def reciprocal_tracker():
+        raise NotImplementedError
 
 class OpsTracker(Tracker):
-  @staticmethod
-  def source_flags_for_search():
-    return [b"OPS", b"APL"]
+    @staticmethod
+    def source_flags_for_search():
+        return [b"OPS", b"APL"]
 
-  @staticmethod
-  def source_flags_for_creation():
-    return [b"OPS", b"APL", b""]
+    @staticmethod
+    def source_flags_for_creation():
+        return [b"OPS", b"APL", b""]
 
-  @staticmethod
-  def announce_url():
-    return b"home.opsfet.ch"
+    @staticmethod
+    def announce_url():
+        return "https://home.opsfet.ch/announce"
 
-  @staticmethod
-  def site_shortname():
-    return "OPS"
+    @staticmethod
+    def site_shortname():
+        return "OPS"
 
-  @staticmethod
-  def reciprocal_tracker():
-    return RedTracker
-
+    @staticmethod
+    def reciprocal_tracker():
+        return RedTracker
 
 class RedTracker(Tracker):
-  @staticmethod
-  def source_flags_for_search():
-    return [b"RED", b"PTH"]
+    @staticmethod
+    def source_flags_for_search():
+        return [b"RED", b"PTH"]
 
-  @staticmethod
-  def source_flags_for_creation():
-    return [b"RED", b"PTH", b""]
+    @staticmethod
+    def source_flags_for_creation():
+        return [b"RED", b"PTH", b""]
 
-  @staticmethod
-  def announce_url():
-    return b"flacsfor.me"
+    @staticmethod
+    def announce_url():
+        return "https://flacsfor.me/announce"
 
-  @staticmethod
-  def site_shortname():
-    return "RED"
+    @staticmethod
+    def site_shortname():
+        return "RED"
 
-  @staticmethod
-  def reciprocal_tracker():
-    return OpsTracker
+    @staticmethod
+    def reciprocal_tracker():
+        return OpsTracker
+
+# Consistently handle API responses by using the GazelleAPI class
+# Simplify output filepath generation logic by removing the need for it in this context
