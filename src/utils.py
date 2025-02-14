51d8a9015afca5a38abdb7fca936f4da316b431f
vatsal_venkatkrishna <vatsal_venkatkrishna@gcpl4-eu-0>
2025-02-14 13:03:33 +0000

    Reset to 24479d0341ce3fd94ec20fe71d70e6c2fad70c50 and made changes for LLM Round 1

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,1 @@
-def flatten(arg):
-  if not isinstance(arg, list):
-    return [arg]
-  return [x for sub in arg for x in flatten(sub)]
-
-
-def url_join(*args):
-  return "/".join([str(arg).strip("/") for arg in args if str(arg).strip("/")])
+from src.utils import url_join\\ndef flatten(arg):\\  if not isinstance(arg, list):\\    return [arg]\\  return [x for sub in arg for x in flatten(sub)]
