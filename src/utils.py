0c11dda5ccadaad1ba99c0fb3d7433fbab22d4a9
vatsal_venkatkrishna <vatsal_venkatkrishna@gcpl4-eu-0>
2025-02-14 13:37:43 +0000

    Reset to 24479d0341ce3fd94ec20fe71d70e6c2fad70c50 and made changes for LLM Round 1

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,15 @@
+from src.utils import url_join
+from src.filesystem import sane_join
+
 def flatten(arg):
-  if not isinstance(arg, list):
-    return [arg]
-  return [x for sub in arg for x in flatten(sub)]
+    if not isinstance(arg, list):
+        return [arg]
+    return [x for sub in arg for x in flatten(sub)]
 
+# Using url_join for URL handling
+def join_urls(*args):
+    return url_join(*args)
 
-def url_join(*args):
-  return "/".join([str(arg).strip("/") for arg in args if str(arg).strip("/")])
+# Using sane_join for path joining
+def join_paths(*args):
+    return sane_join(*args)
