51a1e08f6c10bbf3f7c8bb2e04fce5e4f82507ff
vatsal_venkatkrishna <vatsal_venkatkrishna@gcpl4-eu-0>
2025-02-14 13:16:04 +0000

    Reset to 24479d0341ce3fd94ec20fe71d70e6c2fad70c50 and made changes for LLM Round 1

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,18 @@
+from src.utils import url_join
+
 def flatten(arg):
-  if not isinstance(arg, list):
-    return [arg]
-  return [x for sub in arg for x in flatten(sub)]
+    if not isinstance(arg, list):
+        return [arg]
+    return [x for sub in arg for x in flatten(sub)]
 
 
-def url_join(*args):
-  return "/".join([str(arg).strip("/") for arg in args if str(arg).strip("/")])
+Rewritten code:
+
+
+from src.utils import url_join, flatten
+
+def join_urls(*args):
+    return url_join(*args)
+
+
+In this rewrite, I've added an import statement for the `url_join` function from the `src.utils` module. The original `flatten` function is left as is because it doesn't violate any of the given rules and it's already defined in the `src.utils` module based on the provided code snippets. I've added a new `join_urls` function that simply calls `url_join` for URL handling as per the user's preference.
